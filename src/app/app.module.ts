93ab9b03fb700a6b594ee642cee0732f7ac2864e
Santhosh Patnam <santhoshpatnam@gmail.com>
2021-01-01 19:24:08 +0530

    line chart changes

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LineChartComponent } from './line-chart/line-chart.component';
+import { ChartsModule } from 'ng2-charts';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LineChartComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ChartsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
